f4275d8102a6e0dcfc9c3c2c45effa72e944875c
Dmitrii Kasnitskii <buildog@mail.ru>
2024-01-23 02:15:25 +0300

    fix: change signature 5 task for 6 task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,25 +1,30 @@
 const fs = require('fs');
 const path = require('path');
 
-const stylesFolderPath = path.join(__dirname, 'styles');
-const stylesFiles = fs.readdirSync(stylesFolderPath);
+function mergeStyles(from, to) {
+  const stylesFolderPath = from ? from : path.join(__dirname, 'styles');
+  const stylesFiles = fs.readdirSync(stylesFolderPath);
 
-const validExtensions = ['.css'];
-const styleFileNames = stylesFiles.filter(file => {
-  const extension = path.extname(file);
-  const isFile = fs.statSync(path.join(stylesFolderPath, file)).isFile();
-  return isFile && validExtensions.includes(extension);
-});
+  const validExtensions = ['.css'];
+  const styleFileNames = stylesFiles.filter(file => {
+    const extension = path.extname(file);
+    const isFile = fs.statSync(path.join(stylesFolderPath, file)).isFile();
+    return isFile && validExtensions.includes(extension);
+  });
 
-const readStyleFile = (fileName) => {
-  const filePath = path.join(stylesFolderPath, fileName);
-  return fs.readFileSync(filePath, 'utf-8');
-};
+  const readStyleFile = (fileName) => {
+    const filePath = path.join(stylesFolderPath, fileName);
+    return fs.readFileSync(filePath, 'utf-8');
+  };
 
-const stylesArray = styleFileNames.map(fileName => readStyleFile(fileName));
+  const stylesArray = styleFileNames.map(fileName => readStyleFile(fileName));
 
-const bundleFilePath = path.join(__dirname, 'project-dist', 'bundle.css');
-fs.writeFileSync(bundleFilePath, stylesArray.join('\n'));
+  const bundleFilePath = to ? to : path.join(__dirname, 'project-dist', 'bundle.css');
+  fs.writeFileSync(bundleFilePath, stylesArray.join('\n'));
 
-console.log('Bundle created successfully.');
+  console.log('Bundle created successfully.');
+}
 
+mergeStyles();
+
+module.exports.mergeStyles = mergeStyles;
