02f45ff6e72af67e8c9becb2ebfdea7b36539d2a
Dmitrii Kasnitskii <buildog@mail.ru>
2024-01-23 00:53:32 +0300

    fix: remove sync functions

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,28 +1,42 @@
 const fs = require('fs');
 const path = require('path');
 
-const copyFolder = path.join(__dirname, 'files-copy');
-if (!fs.existsSync(copyFolder)) {
-  fs.mkdirSync(copyFolder);
-}
+async function copyDirectory(from, to) {
+  const copyFolder = to ? to : path.join(__dirname, 'files-copy');
+  const filesFolder = from ? from : path.join(__dirname, 'files');
 
-const filesFolder = path.join(__dirname, 'files');
-fs.readdir(filesFolder, (err, files) => {
-  if (err) {
-    console.error('Error reading files folder:', err);
-    return;
-  }
-
-  files.forEach((file) => {
-    const sourcePath = path.join(filesFolder, file);
-    const destinationPath = path.join(copyFolder, file);
-
-    const readStream = fs.createReadStream(sourcePath);
-
-    const writeStream = fs.createWriteStream(destinationPath);
-
-    readStream.pipe(writeStream);
+  await fs.mkdir(copyFolder, { recursive: false }, () => {
   });
 
-  console.log('Files copied successfully!');
-});
+  await fs.readdir(copyFolder, (err, files) => {
+    for (const file of files) {
+      fs.unlink(path.join(copyFolder, file), (err) => {
+        if (err) throw err;
+      });
+    }
+  });
+
+  await fs.readdir(filesFolder, (err, files) => {
+    if (err) {
+      console.error('Error reading files folder:', err);
+      return;
+    }
+
+    files.forEach((file) => {
+      const sourcePath = path.join(filesFolder, file);
+      const destinationPath = path.join(copyFolder, file);
+
+      const readStream = fs.createReadStream(sourcePath);
+
+      const writeStream = fs.createWriteStream(destinationPath);
+
+      readStream.pipe(writeStream);
+    });
+
+    console.log('Files copied successfully!');
+  });
+}
+
+copyDirectory();
+
+module.exports.copyDirectory = copyDirectory;
