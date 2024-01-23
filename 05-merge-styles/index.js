2fea162eb3f226368fb9d9e28751d1004fda4452
Dmitrii Kasnitskii <buildog@mail.ru>
2024-01-23 20:30:03 +0300

    fix: remove sync fs

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -3,27 +3,52 @@ const path = require('path');
 
 function mergeStyles(from, to) {
   const stylesFolderPath = from ? from : path.join(__dirname, 'styles');
-  const stylesFiles = fs.readdirSync(stylesFolderPath);
+  fs.readdir(stylesFolderPath, (err, stylesFiles) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
 
-  const validExtensions = ['.css'];
-  const styleFileNames = stylesFiles.filter(file => {
-    const extension = path.extname(file);
-    const isFile = fs.statSync(path.join(stylesFolderPath, file)).isFile();
-    return isFile && validExtensions.includes(extension);
+    const validExtensions = ['.css'];
+    const styleFileNames = stylesFiles.filter(file => {
+      const extension = path.extname(file);
+      return validExtensions.includes(extension);
+    });
+
+    const readStyleFile = (fileName, callback) => {
+      const filePath = path.join(stylesFolderPath, fileName);
+      fs.readFile(filePath, 'utf-8', (err, data) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        callback(data);
+      });
+    };
+
+    const stylesArray = [];
+    let readFileCount = 0;
+
+    styleFileNames.forEach(fileName => {
+      readStyleFile(fileName, data => {
+        stylesArray.push(data);
+        readFileCount++;
+        if (readFileCount === styleFileNames.length) {
+          const bundleFilePath = to ? to : path.join(__dirname, 'project-dist', 'bundle.css');
+          fs.writeFile(bundleFilePath, stylesArray.join('\n'), err => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+            console.log('Bundle created successfully.');
+          });
+        }
+      });
+    });
   });
-
-  const readStyleFile = (fileName) => {
-    const filePath = path.join(stylesFolderPath, fileName);
-    return fs.readFileSync(filePath, 'utf-8');
-  };
-
-  const stylesArray = styleFileNames.map(fileName => readStyleFile(fileName));
-
-  const bundleFilePath = to ? to : path.join(__dirname, 'project-dist', 'bundle.css');
-  fs.writeFileSync(bundleFilePath, stylesArray.join('\n'));
-
-  console.log('Bundle created successfully.');
 }
+
+
 
 mergeStyles();
 
